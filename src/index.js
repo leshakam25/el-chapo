c28092f395e8965b43b88bb9affff40ccfcec1e4
leshakam25 <leshakam25@gmail.com>
2022-08-02 09:31:55 +0300

    remove empresso

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./fonts/oswald/stylesheet.css";
-import "./fonts/empresso/stylesheet.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
